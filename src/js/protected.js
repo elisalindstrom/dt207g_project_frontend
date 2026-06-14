c7b4b683b57b2746c5195eceed5c9f526b0aa50e
elisalindstrom <elisalindstroem@gmail.com>
2026-06-14 11:28:20 +0200

    lagt till funktion för utskrift av meny + funktion för borttagning av rätt från meny

--- src/js/protected.js
+++ src/js/protected.js
@@ -6,16 +6,17 @@ const itemDescription = document.querySelector("#description");
 const itemPrice = document.querySelector("#price");
 const message = document.querySelector("#message");
 const confirmation = document.querySelector("#confirmation");
-
-addForm.addEventListener("submit", createItem);
-
-// Utloggning
 const logoutBtn = document.querySelector("#logout-btn");
 
+getMenu();
+
+// Utloggning
 logoutBtn.addEventListener("click", () => {
     sessionStorage.removeItem("user_token");
     window.location.href = ("index.html");
 })
+
+addForm.addEventListener("submit", createItem);
 
 // Skapa ny rätt
 async function createItem(event) {
@@ -48,10 +49,8 @@ async function createItem(event) {
 
         if (response.ok) {
             confirmation.textContent = "Ny rätt skapad";
-            console.log(item);
-            console.log(await response.text());
-
             addForm.reset();
+            getMenu();
         } else {
             throw error;
         }
@@ -59,3 +58,78 @@ async function createItem(event) {
         message.textContent = "Rätten kunde inte skapas";
     }
 }
+
+// Hämta meny
+async function getMenu() {
+    try {
+        const response = await fetch("https://dt207g-project-backend-hbda.onrender.com/api/menu");
+        const menu = await response.json();
+
+        if (!menu) return;
+
+        displayMenuAdmin(menu);
+    } catch (error) {
+        console.error("Något gick fel:" + error)
+    }
+}
+
+// Skriv ut meny
+function displayMenuAdmin(menu) {
+    const menuList = document.querySelector(".menu-list");
+    menuList.innerHTML = "";
+
+    menu.forEach(item => {
+        const liEl = document.createElement("li");
+        liEl.classList.add("list-item");
+
+        const spanContentEl = document.createElement("span");
+        const spanButtonsEl = document.createElement("span");
+        spanButtonsEl.classList.add("span-btn");
+
+        const titleEl = document.createElement("h3");
+        titleEl.textContent = item.title;
+
+        const descriptionEl = document.createElement("p");
+        descriptionEl.textContent = item.description;
+
+        const priceEl = document.createElement("p");
+        priceEl.textContent = item.price;
+
+        const changeBtn = document.createElement("button")
+        changeBtn.textContent = "Ändra";
+        changeBtn.classList.add("btn", "primary-btn");
+
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Ta bort";
+        deleteBtn.classList.add("btn", "delete-btn");
+
+        spanContentEl.append(titleEl, descriptionEl, priceEl);
+        spanButtonsEl.append(changeBtn, deleteBtn);
+        liEl.append(spanContentEl, spanButtonsEl);
+        menuList.appendChild(liEl);
+
+        deleteBtn.addEventListener("click", () => {
+            deleteMenuItem(item._id);
+        })
+    })
+}
+
+// Ta bort en rätt från menyn
+async function deleteMenuItem(id) {
+    const userToken = sessionStorage.getItem("user_token");
+    try {
+        const response = await fetch(`https://dt207g-project-backend-hbda.onrender.com/api/menu/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${userToken}`
+            }
+        });
+
+        if (response.ok) {
+            getMenu();
+        }
+    } catch (error) {
+        console.error("Något gick fel:" + error);
+    }
+}
+
